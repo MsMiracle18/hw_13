64c08578685fef46661f19158b7e45230dce7197
Lera <128050534+MsMiracle18@users.noreply.github.com>
2023-10-21 13:37:05 +0200

    Update main.py

--- hw_12-main/main.py
+++ hw_12-main/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, , HTTPException, Request
+from fastapi.middleware.throttle import Throttle, SimpleThrottleLimiter
 
 from app.routes import users
 
@@ -7,7 +8,13 @@ app = FastAPI()
 app.include_router(users.router, prefix='/api')
 app.include_router(auth.router, prefix='/api')
 
-
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+# обмежити кількість запитів до 10 на хвилину на IP-адресу
+app.add_middleware(Throttle, throttler=SimpleThrottleLimiter(rate="10/minute"))
+
+@app.get("/contacts/")
+async def get_contacts(request: Request):
+    return {"message": "Contacts retrieved"}
